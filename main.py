0df84aadc20bbaeba74605f9e02d672ccbf2e00b
wilsonrivero <wilsonrivero2002@gmail.com>
2021-04-27 10:42:58 -0300

    Add input

--- main.py
+++ main.py
@@ -5,15 +5,24 @@ from tkinter import *
 # print(pokemon.sprites[0]["default"])
 # print(pokemon)
 
+def pokemon(name):
+   p = pypokedex.get(name=name)
+   print(p)
+
+
 def main():
    #Inite module
    screen = Tk()
    #window
    screen.title('Pokemon Guide')
    screen.geometry('300x300+800+350')
+   
+   #ENTRY DATA
+   name_pokemon = Entry(screen, bd=1)
+   name_pokemon.place(x=90, y=150)
 
    #Button
-   btn = Button(screen, text='Search')
+   btn = Button(screen, text='Search', command=pokemon)
    btn.place(x=125, y=250)
 
 
