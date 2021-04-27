5c3e2e687f68b25cfe84ff5be1706adf25c3af66
wilsonrivero <wilsonrivero2002@gmail.com>
2021-04-27 11:02:22 -0300

    add def pokemon and getvalueinput

--- main.py
+++ main.py
@@ -5,29 +5,29 @@ from tkinter import *
 # print(pokemon.sprites[0]["default"])
 # print(pokemon)
 
-def pokemon(name):
-   p = pypokedex.get(name=name)
+def pokemon(value):
+   p = pypokedex.get(name=value)
    print(p)
 
+def getValueInput():
+   value = name_pokemon.get()
+   return value
 
-def main():
-   #Inite module
-   screen = Tk()
-   #window
-   screen.title('Pokemon Guide')
-   screen.geometry('300x300+800+350')
-   
-   #ENTRY DATA
-   name_pokemon = Entry(screen, bd=1)
-   name_pokemon.place(x=90, y=150)
+#Inite module
+screen = Tk()
+#window
+screen.title('Pokemon Guide')
+screen.geometry('300x300+800+350')
 
-   #Button
-   btn = Button(screen, text='Search', command=pokemon)
-   btn.place(x=125, y=250)
+#ENTRY DATA
+name_pokemon = Entry(screen, bd=1)
+name_pokemon.place(x=90, y=150)
 
-
-
-   screen.mainloop()
+#Button
+btn = Button(screen, text='Search', command=lambda: pokemon(getValueInput()))
+btn.pack()
+btn.place(x=125, y=250)
 
 
-main()
+screen.mainloop()
+
